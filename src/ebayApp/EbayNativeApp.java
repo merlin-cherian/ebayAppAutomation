77c943b014cb67e6f788127671cbb5c8a29e29d5
Merlin_Cherian <Merlin_Cherian@192.168.8.101>
2018-07-14 00:02:28 +0530

    v0.0 Initial draft

--- src/ebayApp/EbayNativeApp.java
+++ src/ebayApp/EbayNativeApp.java
@@ -1,13 +1,8 @@
 package ebayApp;
 
 import io.appium.java_client.android.AndroidDriver;
-
 import java.net.MalformedURLException;
-import java.net.URL;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -15,36 +10,29 @@ import org.testng.annotations.Test;
 public class EbayNativeApp {
 	
 	AndroidDriver app;
+	String deviceName = "emulator-5554";
+	String platformName = "Android";
+	String apkPath = "D:\\Appium_Req\\eBay.apk";
+	String appPackage = "com.ebay.mobile";
+	String appActivity = ".activities.eBay";
+	String searchText = "65 inch TV";
 	
 	@BeforeClass
 	public void Launchapp() throws MalformedURLException, InterruptedException{
-		
-	DesiredCapabilities capabilities=new DesiredCapabilities();
-	//Platform in which the test is running [Windows, Ubuntu]
-	capabilities.setCapability(CapabilityType.PLATFORM, "WINDOWS");
-	//Android version of the emulator 
-	capabilities.setCapability(CapabilityType.VERSION, "4.4.2");
-	capabilities.setCapability("deviceName", "Android Emulator");
-	capabilities.setCapability("platformName", "Android");
-	capabilities.setCapability("platformName", "Android");
+		setCapabilities launch = new setCapabilities();
+		app = launch.setSessionCapabilities(
+				deviceName,platformName,apkPath,appPackage,appActivity);
 
-	//Specify fully qualified path of apk file
-	capabilities.setCapability("app", "D:\\apk\\");
-	//Specify appPackage and appActivity of the app to be tested
-	capabilities.setCapability("appPackage", "com.ebay.mobile");
-	capabilities.setCapability("appActivity", ".activities.eBay");
-	//Default time the device waits for new command
-	capabilities.setCapability("newCommandTimeout", "60");
-	app =new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-}
+	}
 	
 	@Test 
 	public void Login(){ 
-		app.findElement(By.id("com.ebay.mobile:id/home_sign_in_image"));
-		app.findElementById("com.ebay.mobile:id/home_sign_in_image");
-		app.findElementById("com.ebay.mobile:id/home_search_text").click();
-		app.findElementById("android:id/search_src_text").sendKeys("65 inch TV");
-		app.findElementById("android:id/search_go_btn").click();	
+		LoginPageObjects page = new LoginPageObjects();
+		app.findElement(By.id(page.getSignInImage()));
+		app.findElementById(page.getSignInImage());
+		app.findElementById(page.getSearchField()).click();
+		app.findElementById(page.getSearchTextBox()).sendKeys(searchText);
+		app.findElementById(page.getSearchButton()).click();	
 	}
 	
 	@AfterClass
