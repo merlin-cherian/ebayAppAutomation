b41c337c5762c9373eb8abd60f37d40d6764a886
Merlin_Cherian <Merlin_Cherian@192.168.8.101>
2018-07-14 00:03:30 +0530

    V0.0 initial draft

---
+++ src/ebayApp/LoginPageObjects.java
@@ -0,0 +1,35 @@
+package ebayApp;
+
+public class LoginPageObjects {
+	
+	String signInImage = "com.ebay.mobile:id/home_sign_in_image";
+	String searchField = "com.ebay.mobile:id/home_search_text";
+	String searchTextBox = "android:id/search_src_text";
+	String searchButton = "android:id/search_go_btn";
+	
+	public String getSignInImage() {
+		return signInImage;
+	}
+	public void setSignInImage(String signInImage) {
+		this.signInImage = signInImage;
+	}
+	public String getSearchField() {
+		return searchField;
+	}
+	public void setSearchField(String searchField) {
+		this.searchField = searchField;
+	}
+	public String getSearchTextBox() {
+		return searchTextBox;
+	}
+	public void setSearchTextBox(String searchTextBox) {
+		this.searchTextBox = searchTextBox;
+	}
+	public String getSearchButton() {
+		return searchButton;
+	}
+	public void setSearchButton(String searchButton) {
+		this.searchButton = searchButton;
+	}
+
+}
---
+++ src/ebayApp/setCapabilities.java
@@ -0,0 +1,36 @@
+package ebayApp;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class setCapabilities {
+	
+	public AndroidDriver setSessionCapabilities(String deviceName,String platformName, String app,String appPackage,String appActivity) throws MalformedURLException
+	{
+		AndroidDriver appDriver;
+		DesiredCapabilities capabilities=new DesiredCapabilities();
+		//Platform in which the test is running [Windows, Ubuntu]
+		capabilities.setCapability(CapabilityType.PLATFORM, "WINDOWS");
+		//Android version of the emulator 
+		capabilities.setCapability(CapabilityType.VERSION, "5.5.1");
+		capabilities.setCapability("deviceName", deviceName);
+		capabilities.setCapability("platformName", platformName);
+	
+		//Specify fully qualified path of apk file
+		capabilities.setCapability("app", app);
+		//Specify appPackage and appActivity of the app to be tested
+		capabilities.setCapability("appPackage",appPackage );
+		capabilities.setCapability("appActivity",appActivity );
+		//Default time the device waits for new command
+		capabilities.setCapability("newCommandTimeout", "60");
+		appDriver =new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+		System.out.println("after creating driver");
+		return appDriver;
+	}
+
+}
