9b43ef67b22b1125a441ce5da69f0c1ac35f7337
Merlin_Cherian <Merlin_Cherian@192.168.8.101>
2018-07-14 00:26:15 +0530

    v0.2 Added Product screen objects

---
+++ src/ebayApp/ProductScreenObjects.java
@@ -0,0 +1,49 @@
+package ebayApp;
+
+public class ProductScreenObjects {
+
+	String name;
+	String price;
+	String description;
+	
+	String nameID;
+	String priceID;
+	String descID;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getNameID() {
+		return nameID;
+	}
+	public void setNameID(String nameID) {
+		this.nameID = nameID;
+	}
+	public String getPriceID() {
+		return priceID;
+	}
+	public void setPriceID(String priceID) {
+		this.priceID = priceID;
+	}
+	public String getDescID() {
+		return descID;
+	}
+	public void setDescID(String descID) {
+		this.descID = descID;
+	}
+}
