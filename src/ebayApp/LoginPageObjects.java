2980ffd511ad2a9ce4865a4d79f130f521d6ad37
Merlin_Cherian <Merlin_Cherian@TVMATP95695L.ad.infosys.com>
2018-07-14 13:36:21 +0530

    Modifications

--- src/ebayApp/LoginPageObjects.java
+++ src/ebayApp/LoginPageObjects.java
@@ -4,6 +4,7 @@ public class LoginPageObjects {
 	
 	String signInImage = "com.ebay.mobile:id/home_sign_in_image";
 	String searchField = "com.ebay.mobile:id/home_search_text";
+	//com.ebay.mobile:id/search_box
 	String searchTextBox = "android:id/search_src_text";
 	String searchButton = "android:id/search_go_btn";
 	
