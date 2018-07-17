521e46889405735b42fdad7155bfd548104440f9
Merlin_Cherian <Merlin_Cherian@192.168.8.100>
2018-07-17 09:07:27 +0530

    changes

--- src/ebayApp/EbayNativeApp.java
+++ src/ebayApp/EbayNativeApp.java
@@ -1,15 +1,24 @@
 package ebayApp;
 
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidKeyCode;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.interactions.touch.TouchActions;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -20,7 +29,7 @@ import org.apache.poi.ss.usermodel.Cell;
 
 public class EbayNativeApp {
 	
-	AndroidDriver appDriver;
+	AndroidDriver<MobileElement> appDriver;
     XSSFWorkbook workbook;
     XSSFSheet sheet;
     XSSFCell cell;
@@ -35,7 +44,7 @@ public class EbayNativeApp {
 	public void Launchapp() throws MalformedURLException, InterruptedException{
 		
 		DesiredCapabilities capabilities=new DesiredCapabilities();
-		//Platform in which the test is running [Windows, Ubuntu]
+		//Platform in which the test is running 
 		capabilities.setCapability(CapabilityType.PLATFORM, "WINDOWS");
 		//Android version of the emulator 
 		capabilities.setCapability(CapabilityType.VERSION, "5.5.1");
@@ -49,42 +58,64 @@ public class EbayNativeApp {
 		capabilities.setCapability("appActivity",appActivity);
 		
 		//Default time the device waits for new command
-		capabilities.setCapability("newCommandTimeout", "60");
+		capabilities.setCapability("newCommandTimeout", 60);
 		System.out.println("before creating driver");
-		appDriver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+		appDriver = new AndroidDriver<MobileElement> (new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 		System.out.println("after creating driver");
 	}
 	
 	@Test 
-	public void Login() throws IOException{ 
+	public void Login() throws IOException, InterruptedException{ 
 		
 		// Import excel sheet.
 		File src=new File(inputFilePath);
 		FileInputStream finput = new FileInputStream(src);
+		CommonUtilities common = new CommonUtilities();
 		LoginPageObjects page = new LoginPageObjects();
 		
-		System.out.println("nnnnn");
-		//appDriver.findElement(By.id(page.getSignInImage()));
-		//appDriver.findElementById(page.getSignInImage());
+		System.out.println("Logging in!");
+		//click on Options
+		MobileElement options = (MobileElement) appDriver.findElementById(page.getOptionsImageId());
+		options.click();
+		MobileElement signIn = (MobileElement) appDriver.findElementById(page.getSignInImageId());
+		signIn.click();
 		
-		//appDriver.findElementById(page.getSearchTextBox()).click();
+		//Enter the credentials and Sign in
+		MobileElement userName = (MobileElement) appDriver.findElementById(page.getUserNameId());
+		userName.sendKeys(common.readProperties("username"));
+		MobileElement password = (MobileElement) appDriver.findElementById(page.getPasswordId());
+		password.sendKeys(common.readProperties("password"));
+
+		MobileElement signInButton = (MobileElement) appDriver.findElementById(page.getSignInButtonId());
+		signInButton.click();
 			
-		 // Load the workbook and data sheet
+		//Wait for page to load
+		WebDriverWait wait = new WebDriverWait(appDriver,30);
+		wait.until(ExpectedConditions.elementToBeClickable(By.id(page.getSignInImageId())));
+		
+		// Load the workbook and data sheet
 		 workbook = new XSSFWorkbook(finput); 
 		 sheet= workbook.getSheetAt(0);
 		 
-		 for(int i=1; i<=sheet.getLastRowNum(); i++)
+		 for(int i=0; i<sheet.getLastRowNum(); i++)
 		 {
 			// Import data for Search Text
 			cell = sheet.getRow(i).getCell(1);
 			cell.setCellType(Cell.CELL_TYPE_STRING);
 			System.out.println("data : " + cell.getStringCellValue());
-			appDriver.findElementById(page.getSearchTextBox()).sendKeys(cell.getStringCellValue());		   		
-	      
-			appDriver.findElementById(page.getSearchButton()).click();
+			
+			appDriver.findElementById(page.getSearchTextBoxId()).click();	
+			appDriver.findElementById(page.getSearchBoxId()).sendKeys(cell.getStringCellValue());
+			appDriver.pressKeyCode(AndroidKeyCode.KEYCODE_ENTER);
+			
+			//Wait for Page loading
+			wait(40);
 			
 			//scroll down
-//			appDriver.swipe(0, 30, 0, 60, 2000);
+			JavascriptExecutor js = (JavascriptExecutor) appDriver;
+			HashMap<String,String> scrollObject = new HashMap<String,String>();
+			scrollObject.put("direction", "down");
+			js.executeScript("mobile:scroll", scrollObject);
 			
 			//getting info on product search screen
 //			ProductScreenObjects pscr = new ProductScreenObjects();
--- src/ebayApp/LoginPageObjects.java
+++ src/ebayApp/LoginPageObjects.java
@@ -2,35 +2,57 @@ package ebayApp;
 
 public class LoginPageObjects {
 	
-	String signInImage = "com.ebay.mobile:id/home_sign_in_image";
-	String searchField = "com.ebay.mobile:id/home_search_text";
-	//com.ebay.mobile:id/search_box
-	String searchTextBox = "com.ebay.mobile:id/search_";
-	String searchButton = "android:id/search_go_btn";
+	String optionsImageId = "com.ebay.mobile:id/home";
+	String signInImageId ="com.ebay.mobile:id/logo";
 	
-	public String getSignInImage() {
-		return signInImage;
+	String userNameId = "com.ebay.mobile:id/edit_text_username";
+	String passwordId = "com.ebay.mobile:id/edit_text_password";
+	String signInButtonId = "com.ebay.mobile:id/button_sign_in";
+	
+	String searchTextBoxId = "com.ebay.mobile:id/search_box";
+	String searchBoxId = "com.ebay.mobile:id/search_src_text";
+	
+	public String getOptionsImageId() {
+		return optionsImageId;
 	}
-	public void setSignInImage(String signInImage) {
-		this.signInImage = signInImage;
+	public void setOptionsImageId(String optionsImageId) {
+		this.optionsImageId = optionsImageId;
 	}
-	public String getSearchField() {
-		return searchField;
+	public String getSignInImageId() {
+		return signInImageId;
 	}
-	public void setSearchField(String searchField) {
-		this.searchField = searchField;
+	public void setSignInImageId(String signInImageId) {
+		this.signInImageId = signInImageId;
 	}
-	public String getSearchTextBox() {
-		return searchTextBox;
+	public String getUserNameId() {
+		return userNameId;
 	}
-	public void setSearchTextBox(String searchTextBox) {
-		this.searchTextBox = searchTextBox;
+	public void setUserNameId(String userNameId) {
+		this.userNameId = userNameId;
 	}
-	public String getSearchButton() {
-		return searchButton;
+	public String getPasswordId() {
+		return passwordId;
 	}
-	public void setSearchButton(String searchButton) {
-		this.searchButton = searchButton;
+	public void setPasswordId(String passwordId) {
+		this.passwordId = passwordId;
+	}
+	public String getSignInButtonId() {
+		return signInButtonId;
+	}
+	public void setSignInButtonId(String signInButtonId) {
+		this.signInButtonId = signInButtonId;
+	}
+	public String getSearchTextBoxId() {
+		return searchTextBoxId;
+	}
+	public void setSearchTextBoxId(String searchTextBoxId) {
+		this.searchTextBoxId = searchTextBoxId;
+	}
+	public String getSearchBoxId() {
+		return searchBoxId;
+	}
+	public void setSearchBoxId(String searchBoxId) {
+		this.searchBoxId = searchBoxId;
 	}
 
 }
